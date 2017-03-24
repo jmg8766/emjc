1301b602cf7939956c4a456d75d6911c27a9395c
dnaed <de9901@rit.edu>
2017-03-24 15:01:27 -0400

    Adding nested expression test

--- src/test/java/ParserTest.java
+++ src/test/java/ParserTest.java
@@ -5,11 +5,19 @@ import java.io.File;
 @Test(dataProviderClass = Providers.class)
 public class ParserTest {
 
-	@Test(dataProvider = "benchmarkFiles")
-	void testGenAstFile(File f) {
+//	@Test(dataProvider = "benchmarkFiles")
+//	void testGenAstFile(File f) {
+//		System.out.println("Generating ast file for " + f.getName());
+//		Emjc.main(new String[] {"--ast", f.getPath()});
+//	}
+
+	@Test
+	void testAstSingleFile() {
+		File f = new File("src/test/benchmarks/Simple.emj");
 		System.out.println("Generating ast file for " + f.getName());
 		Emjc.main(new String[] {"--ast", f.getPath()});
 	}
+
 
 	@Test
 	public void testParseProgram() throws Exception {
