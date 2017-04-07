a11c1aaeee84f8c45959ae00dd75ffc240032be5
dnaed <de9901@rit.edu>
2017-04-07 14:16:06 -0400

    Semantic test

---
+++ src/test/java/SemanticTest.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+@Test(dataProviderClass = Providers.class)
+public class SemanticTest {
+
+    @Test(dataProvider = "benchmarkFiles", enabled = false)
+    void testGenAstFile(File f) {
+        System.out.println("Performing Name analysis: " + f.getName());
+        Emjc.main(new String[] {"--name", f.getPath()});
+    }
+
+    @Test(enabled = true)
+    void testAstSingleFile() {
+        File f = new File("src/test/benchmarks-old/Factorial.emj");
+        System.out.println("Performing Name analysis: " + f.getName());
+        Emjc.main(new String[] {"--name", f.getPath()});
+    }
+}
