8952ce6506401acf0866465b9e8c1672c7bf5acf
jmg <jmg8766@rit.edu>
2017-04-07 14:13:40 -0400

    comments and formatting to SymbolTable

--- src/main/java/symbol/SymbolTable.java
+++ src/main/java/symbol/SymbolTable.java
@@ -11,17 +11,21 @@ public class SymbolTable<V> {
 	private final LinkedList<HashMap<String, V>> symbols = new LinkedList<>();
 
 	/**
-	 * @param key
-	 * @param value
 	 * @return the previous value associated with the key
 	 */
 	public V put(Identifier key, V value) {
 		return symbols.getFirst().put(key.s.intern(), value);
 	}
 
+	/**
+	 * @return the value associated with a key, or null if the key doesn't exist
+	 */
 	public V get(Identifier key) {
-		return symbols.stream().map(m -> m.get(key.s.intern())).filter(Objects::nonNull).findFirst().orElseGet(() ->
-				null);
+		return symbols.stream()
+			.map(m -> m.get(key.s.intern()))
+			.filter(Objects::nonNull)
+			.findFirst()
+			.orElseGet(() -> null);
 	}
 
 	public void beginScope() {
